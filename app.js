f61c1257340290dd0482fc15fdbbdebe4f1218d5
NishatFariza <nishatfarzia78@gmail.com>
2022-02-28 23:58:26 +0600

    search bar done

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert('hhhhhhhhhh')
+const loadAllPhone = ()=>{
+    const allPhoneUrl =(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+    fetch(allPhoneUrl)
+    .then(res => res.json())
+    .then(data =>console.log(data))
+}
+
+loadAllPhone()
