f8e45bc2cf38b8105ed31b1913c6c0ff6fc3ea74
NishatFariza <nishatfarzia78@gmail.com>
2022-03-01 22:14:04 +0600

    error handel

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-const loadAllPhone = ()=>{
-    const searchValue =document.getElementById('search-input').value;
+const searchInput =document.getElementById('search-input');
+const searchResultContainer = document.getElementById('search-result');
+const phoneDetails =document.getElementById('phone-details')
 
-    // console.log(searchValue)
-    const allPhoneUrl =(`https://openapi.programming-hero.com/api/phones?search=${searchValue}`)
-    fetch(allPhoneUrl)
-    .then(res => res.json())
-    .then(data =>searchPhone(data.data, searchValue))
+const loadAllPhone = ()=>{
+  if(isNaN(searchInput.value)==false || searchInput.value==""){
+    searchInput.value="";
+        alert('Enter a vaild input')
+        
+  }
+  else{
+    searchResultContainer.textContent="";
+    phoneDetails.textContent="";
+     // console.log(searchValue)
+     const allPhoneUrl =(`https://openapi.programming-hero.com/api/phones?search=${searchInput.value}`)
+     fetch(allPhoneUrl)
+     .then(res => res.json())
+     .then(data =>{
+       if(data.status ==false){
+         document.getElementById('error-text').style.display="block";
+            
+       }
+       else{
+         searchPhone(data.data)
+        
+       }
+ 
+     })
+     searchInput.value="";
+  }
+  
+
+    
 }
 
 // loadAllPhone()
 
-const searchPhone = (phones, searchValue) =>{
-    console.log(phones)
+const searchPhone = (phones) =>{
+    // console.log(phones)
     // console.log(searchValue); 
     const searchResultContainer = document.getElementById('search-result');
-    phones.forEach(phone => {
+    const topTwentyPhone =phones.slice(0,20)
+    topTwentyPhone.forEach(phone => {
         const {phone_name, image, brand, slug}= phone;
         // console.log(phone);
       const div =document.createElement('div');
@@ -23,19 +49,77 @@ const searchPhone = (phones, searchValue) =>{
       <img class="rounded-3" src="${image}"/>
       <h4 class="mt-5">Phone Name: <span class="favorite-color">${phone_name}<span></h4>
       <h5 class="mt-3">Brand Name: <span class="favorite-color">${brand}<span></h5>
-      <button type="button" onclick="phoneDetails(slug${slug})" class="btn btn-outline-secondary px-4 py-2 mt-4">Explore Now</button>
+      <button type="button" onclick="phoneSlugUrl('${slug}')" class="btn btn-outline-secondary px-4 py-2 mt-4">Explore Now</button>
     </div>
       `
       searchResultContainer.appendChild(div)
     });
+    
 }
 
 
-const phoneDetails = slug =>{
-    const phoneSlug =(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`)
-    fetch(phoneSlug)
+const phoneSlugUrl = slug =>{
+    const slugUrl =(`https://openapi.programming-hero.com/api/phone/${slug}`)
+    fetch(slugUrl)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayPhoneDetails(data.data))
+}
+
+const displayPhoneDetails = details =>{
+    console.log(details)
+    phoneDetails.textContent="";
+        // console.log(phoneDetails);
+      const playerDetailsContainer =document.createElement('div');
+      playerDetailsContainer.classList.add('player-style')
+      const {name, image, brand, mainFeatures, others}= details;
+      if(details.releaseDate==""){
+        details.releaseDate="Relase date is comming soon.";
+      }
+      playerDetailsContainer.innerHTML=`
+         <div class="text-center w-100">
+            <img  class="img-fluid rounded w-25" src="${image}"/>
+          </div>
+          <div class="text-center">
+            <div class="card-body">
+              <h4 class="card-title mb-3">${brand}</h4>
+              <h3 class="card-title favorite-color mb-3 fs-2"> ${name}</h3>
+              <h5 class="card-text">
+                ReleaseDate: ${details.releaseDate}
+              </h5>
+              <p class="card-text">
+                MainFeatures:
+                <ul  class="list-unstyled">
+                <li>Chipset: <span class="text-muted">${mainFeatures.chipSet}</span></li>
+                <li>Display size: <span class="text-muted" class="text-muted">${mainFeatures.displaySize}</span></li>
+                <li>Memory: <span class="text-muted" class="text-muted">${mainFeatures.memory}</span></li>
+                <li>Storage: <span class="text-muted" class="text-muted">${mainFeatures.storage}</span></li>
+                <li>Sensors: <span class="text-muted" class="text-muted">${mainFeatures.sensors}</span></li>
+                </ul>
+              </p>
+            </div>
+          </div>
+      `;
+      phoneDetails.appendChild(playerDetailsContainer)
+
+      if(details.others != undefined){
+       const otherDiv = document.createElement('div')
+       otherDiv.innerHTML=`
+       <h5 class="card-text">
+       Others:
+       <ul class="list-unstyled">
+       <li>Bluetooth: <span class="text-muted">${others.Bluetooth}</span></li>
+       <li>GPS: <span class="text-muted">${others.GPS}</span></li>
+       <li>NFC: <span class="text-muted">${others.NFC}</span></li>
+       <li>Radio: <span class="text-muted">${others.Radio}</span></li>
+       <li>USB: <span class="text-muted">${others.USB}</span></li>
+       <li>WLAN: <span class="text-muted">${others.WLAN}</span></li>
+       </ul>
+     </h5> 
+       `;
+       playerDetailsContainer.appendChild(otherDiv)
+
+
+      }
 }
 
 
